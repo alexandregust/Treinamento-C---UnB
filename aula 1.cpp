4a935db919224b63706d2bce6ea6a2c948eb5a09
alexandregust <gustavo.alexandre3@gmail.com>
2018-03-15 13:41:45 -0300

    Conhecendo a estrutura

---
+++ aula 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Biblioteca de pré processador
+using namespace std; //Fazer abreviação de nomes, ex std :: cout ---> cout
+
+	int main()  
+{
+
+		cout << "Eae galera, tudo bem?" << endl << "Tudo e vc?" << endl; // operação de escrita ---> cout, saida padrão de texto e end line --> endl	
+       
+		return 0; // Encerrar o programa
+}
+
