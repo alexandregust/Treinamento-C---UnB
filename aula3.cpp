953e5832df00375c39cbc5b389b1e55e76b67a2b
Alexandre <gustavo.alexandre3@gmail.com>
2018-03-21 10:55:16 -0300

    entrada e saida

---
+++ aula3.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+	int main () {
+
+		int a, b, media;
+		
+		cin >> a;//Ler o número e guarda em a
+		cin >> b;//Ler o número e guarda em b
+		
+		media = (a + b)/2;
+		
+		cout << media << endl;
+
+
+
+	return 0;}
