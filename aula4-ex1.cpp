bf67df0d15488329737aab067ac8fd39354194fd
Alexandre <gustavo.alexandre3@gmail.com>
2018-03-22 23:31:57 -0300

    multiplicação

---
+++ aula4-ex1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+	
+	int a, b, c, total;
+	
+
+	cin>>a;
+
+	cin>>b;
+	
+	cin>>c;
+	
+	total = a * b * c;
+	
+
+	cout<< total<< endl;
+	
+	return 0;
+	
+	
+	
+}
